62ee48982f7d7c68246446e9e97c8f228253d545
Jay <Jay@163.com>
2019-06-21 18:00:56 +0800

    添加了登录页面和后台管理的路由组件并完成跳转

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@
  * 应用的根组件
  */
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Admin from './pages/admin';
+import Login from './pages/login';
 
 export default class App extends Component {
 
@@ -9,8 +13,16 @@ export default class App extends Component {
 
         return (
             <div>
-				App
-			</div>
+                <BrowserRouter>
+
+                    {/* 当前只执行一个路由组件 */}
+                    <Switch>
+                        {/* 顺序有影响,要注意 */}
+                        <Route path="/login" component={Login}/>
+                        <Route path="/" component={Admin}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
         )
 
     }

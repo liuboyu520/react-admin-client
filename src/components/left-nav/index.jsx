fe3fe7362cee7c8776bebe2f244809fb515d04a4
Jay <Jay@163.com>
2019-06-21 23:13:10 +0800

    完成了管理页面左侧菜单的布局和样式，新增了left-nav和header组件。

---
+++ src/components/left-nav/index.jsx
@@ -0,0 +1,103 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+//引入antd组件
+import {Menu, Icon} from 'antd';
+
+//引入图片
+import logo from '../../assets/images/logo.png';
+
+import './index.less';
+
+const {SubMenu} = Menu;
+
+/**
+ * 左侧菜单
+ */
+export default class LeftNav extends Component {
+
+    render() {
+
+        return (
+            <div className="left-nav">
+                <Link to="/" className="left-nav-header">
+                    <img src={logo} alt="logo"/>
+                    <h2>硅谷后台</h2>
+                </Link>
+                <Menu
+                    mode="inline"
+                    theme="dark">
+
+                    {/* 一级菜单：首页 */}
+                    <Menu.Item >
+                        <Icon type="home"/>
+                        <span>首页</span>
+                    </Menu.Item>
+
+                    {/* 一级菜单：商品 */}
+                    <SubMenu
+                        key="sub1"
+                        title={
+                            <span>
+                                <Icon type="appstore"/>
+                                <span>商品</span>
+                             </span>
+                        }
+                    >
+                        <Menu.Item key="2">
+                            <Icon type="bars"/>
+                            <span>品类管理</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="tool"/>
+                            <span>商品管理</span>
+                        </Menu.Item>
+                    </SubMenu>
+
+                    {/* 一级菜单：用户管理 */}
+                    <SubMenu
+                        key="sub2"
+                        title={
+                            <span>
+                                <Icon type="user"/>
+                                <span>用户管理</span>
+                             </span>
+                        }
+                    >
+                        <Menu.Item key="4">用户管理1</Menu.Item>
+                        <Menu.Item key="5">用户管理2</Menu.Item>
+                    </SubMenu>
+
+                    {/* 一级菜单：角色管理 */}
+                    <SubMenu
+                        key="sub3"
+                        title={
+                            <span>
+                                <Icon type="team"/>
+                                <span>角色管理</span>
+                             </span>
+                        }
+                    >
+                        <Menu.Item key="6">角色管理1</Menu.Item>
+                        <Menu.Item key="7">角色管理2</Menu.Item>
+                    </SubMenu>
+
+                    {/* 一级菜单：图形图表 */}
+                    <SubMenu
+                        key="sub4"
+                        title={
+                            <span>
+                                <Icon type="bar-chart"/>
+                                <span>图形图表</span>
+                             </span>
+                        }
+                    >
+                        <Menu.Item key="8">图形图表1</Menu.Item>
+                        <Menu.Item key="9">图形图表2</Menu.Item>
+                    </SubMenu>
+
+                </Menu>
+            </div>
+        )
+    }
+}
--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import memoryUtils from '../../utils/memoryUtils';
 
+import { Layout  } from 'antd';
+
+//引入自定义组件
+import LeftNav from '../../components/left-nav';
+import Header from '../../components/header';
+
+const { Footer, Sider, Content } = Layout;
+
+/**
+ * 后台管理的路由
+ */
 export default class Admin extends Component {
 
     render(){
-
+        const user = memoryUtils.user;
+        //如果内存中没有存储user(用户没有登录)
+        if(!user || !user._id){
+            //自动跳转到登录页面
+            return <Redirect to="/login"/>
+        }
         return (
-            <div>Admin</div>
+            <Layout style={{ height: '100%' }}>
+                {/* 左侧菜单 */}
+                <Sider>
+                    <LeftNav/>
+                </Sider>
+                <Layout>
+                    <Header>Header</Header>
+                    <Content style={{ backgroundColor: 'pink' }}>Content</Content>
+                    <Footer style={{ textAlign: 'center' }}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
